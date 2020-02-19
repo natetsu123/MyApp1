36c2c7c800e560db8933b2b4601f8aa6e20b19bd
t-nakatani@dr.jp.nec.com <t-nakatani@dr.jp.nec.com>
2020-02-19 17:07:31 +0900

    SQLiteDataAccess クラスの作成

--- src/MySolution1/InfrastructureLayer/DB/SQLite/Entity/Parameter.cs
+++ src/MySolution1/InfrastructureLayer/DB/SQLite/Entity/Parameter.cs
@@ -6,33 +6,11 @@ using System.Threading.Tasks;
 
 namespace InfrastructureLayer.DB.SQLite.Entity
 {
-    /// <summary>
-    /// SQLite Parameter レコードクラス
-    /// </summary>
     public class Parameter
     {
-        #region ===== プロパティ =====
-
-        /// <summary>
-        /// Id
-        /// </summary>
         public int Id { get; set; }
-
-        /// <summary>
-        /// 一意の Key
-        /// </summary>
         public string Key { get; set; }
-
-        /// <summary>
-        /// 設定値
-        /// </summary>
         public string Value { get; set; }
-
-        /// <summary>
-        /// 設定値の説明
-        /// </summary>
         public string Description { get; set; }
-
-        #endregion
     }
 }
